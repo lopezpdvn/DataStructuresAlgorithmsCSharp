fc8e87cfcebf79e900cf7d2c7c4d87755832e8ca
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-07-07 16:11:18 -0500

    Implemented StackSinglyLinkedList

---
+++ data_structures/DataStructures/src/DataStructures/Stack.cs
@@ -0,0 +1,34 @@
+﻿using DataStructures.LinkedList;
+using System;
+using System.Collections.Generic;
+
+namespace AbstractDataType
+{
+    public interface Stack<T>
+    {
+        void Push(T value);
+        T Pop();
+    }
+
+    public class StackSinglyLinkedList<T> : SinglyLinkedList<T>, Stack<T>
+    {
+        public T Pop()
+        {
+            try
+            {
+                T value = FirstNode.Value;
+                RemoveFirst();
+                return value;
+            }
+            catch (NullReferenceException)
+            {
+                throw new InvalidOperationException("Stack is Empty, Pop failed.");
+            }
+        }
+
+        public void Push(T value)
+        {
+            AddFirst(value);
+        }
+    }
+}
