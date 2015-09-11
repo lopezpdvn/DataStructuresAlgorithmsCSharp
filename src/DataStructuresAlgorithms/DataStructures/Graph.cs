f58665679c741299dd7fc59827088eeb49af1041
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-09-10 21:05:21 -0500

    Added Graph namespace sketch.

---
+++ src/DataStructuresAlgorithms/DataStructures/Graph.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructures.Graph
+{
+    public class DirectedGraph<T>
+    {
+        public enum State
+        {
+            Unvisited, Visited
+        }
+
+        public class Node<T>
+        {
+        }
+    }
+}
