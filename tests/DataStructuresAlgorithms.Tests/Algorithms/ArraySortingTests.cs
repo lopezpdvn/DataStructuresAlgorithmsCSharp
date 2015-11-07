b47284f97b0927f8aec2ecc91c7bde7e8c4c1c81
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-11-07 17:34:54 -0600

    Change of terminology in Array Sorting tests

--- tests/DataStructuresAlgorithms.Tests/Algorithms/ArraySortingTests.cs
+++ tests/DataStructuresAlgorithms.Tests/Algorithms/ArraySortingTests.cs
@@ -6,42 +6,42 @@ namespace DataStructuresAlgorithms.Tests.Algorithms
 {
     public class ArraySortingTests
     {
-        int[] unorderedBig, orderedBig, unorderedTwo, orderedTwo, unorderedOne,
-            orderedOne, unorderedThree, orderedThree, unorderedFour, orderedFour,
-            unorderedEmpty, orderedEmpty;
-        int[][] intOrdered, intUnordered;
+        int[] unsortedBig, sortedBig, unsortedTwo, sortedTwo, unsortedOne,
+            sortedOne, unsortedThree, sortedThree, unsortedFour, sortedFour,
+            unsortedEmpty, sortedEmpty;
+        int[][] intSorted, intUnsorted;
 
         public ArraySortingTests()
         {
-            unorderedBig = new int[] { 888, 222, 333, 000, 999, 777, 555, 111, 666, 444 };
-            orderedBig = new int[] { 000, 111, 222, 333, 444, 555, 666, 777, 888, 999 };
-            unorderedTwo = new int[] { 888, 222 };
-            orderedTwo = new int[] { 222, 888 };
-            unorderedOne = new int[] { 999 };
-            orderedOne = new int[] { 999 };
-            unorderedThree = new int[] { 333, 000, 999 };
-            orderedThree = new int[] { 000, 333, 999 };
-            unorderedFour = new int[] { 555, 111, 666, 444 };
-            orderedFour = new int[] {111, 444, 555, 666};
-            unorderedEmpty = new int[] { };
-            orderedEmpty = new int[] { };
-            intOrdered = new int[][] { orderedOne, orderedTwo, orderedThree,
-                orderedFour, orderedBig, orderedEmpty};
-            intUnordered = new int[][] { unorderedOne, unorderedTwo, unorderedThree,
-                unorderedFour, unorderedBig, unorderedEmpty };
+            unsortedBig = new int[] { 888, 222, 333, 000, 999, 777, 555, 111, 666, 444 };
+            sortedBig = new int[] { 000, 111, 222, 333, 444, 555, 666, 777, 888, 999 };
+            unsortedTwo = new int[] { 888, 222 };
+            sortedTwo = new int[] { 222, 888 };
+            unsortedOne = new int[] { 999 };
+            sortedOne = new int[] { 999 };
+            unsortedThree = new int[] { 333, 000, 999 };
+            sortedThree = new int[] { 000, 333, 999 };
+            unsortedFour = new int[] { 555, 111, 666, 444 };
+            sortedFour = new int[] {111, 444, 555, 666};
+            unsortedEmpty = new int[] { };
+            sortedEmpty = new int[] { };
+            intSorted = new int[][] { sortedOne, sortedTwo, sortedThree,
+                sortedFour, sortedBig, sortedEmpty};
+            intUnsorted = new int[][] { unsortedOne, unsortedTwo, unsortedThree,
+                unsortedFour, unsortedBig, unsortedEmpty };
         }
 
         [Fact]
         public void BubbleSortTestInts0()
         {
             var i = 0;
-            foreach(var unorderedArr in intUnordered)
+            foreach(var unsortedArr in intUnsorted)
             {
                 var j = 0;
-                ArraySorting.BubbleSort(unorderedArr);
-                foreach(var value in unorderedArr)
+                ArraySorting.BubbleSort(unsortedArr);
+                foreach(var value in unsortedArr)
                 {
-                    Assert.True(value == intOrdered[i][j++]);
+                    Assert.True(value == intSorted[i][j++]);
                 }
                 i++;
             }
@@ -51,13 +51,13 @@ namespace DataStructuresAlgorithms.Tests.Algorithms
         public void SelectionSortTestInts0()
         {
             var i = 0;
-            foreach (var unorderedArr in intUnordered)
+            foreach (var unsortedArr in intUnsorted)
             {
                 var j = 0;
-                ArraySorting.SelectionSort(unorderedArr);
-                foreach (var value in unorderedArr)
+                ArraySorting.SelectionSort(unsortedArr);
+                foreach (var value in unsortedArr)
                 {
-                    Assert.True(value == intOrdered[i][j++]);
+                    Assert.True(value == intSorted[i][j++]);
                 }
                 i++;
             }
@@ -67,13 +67,13 @@ namespace DataStructuresAlgorithms.Tests.Algorithms
         public void InsertionSortTestInts0()
         {
             var i = 0;
-            foreach (var unorderedArr in intUnordered)
+            foreach (var unsortedArr in intUnsorted)
             {
                 var j = 0;
-                ArraySorting.InsertionSort(unorderedArr);
-                foreach (var value in unorderedArr)
+                ArraySorting.InsertionSort(unsortedArr);
+                foreach (var value in unsortedArr)
                 {
-                    Assert.True(value == intOrdered[i][j++]);
+                    Assert.True(value == intSorted[i][j++]);
                 }
                 i++;
             }
