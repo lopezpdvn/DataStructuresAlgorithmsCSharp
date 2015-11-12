57684848105f3e614456c61cd74a9270d5da559d
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-11-12 15:49:30 -0600

    Refactored test methods into abstract class StackTests
    Test methods are members of abstract class StackTests that uses reference of
    type Stack<T>. Abstract methods allows child classes to provide concrete Stack
    implementations.

--- tests/DataStructuresAlgorithms.Tests/DataStructures/StackTests.cs
+++ tests/DataStructuresAlgorithms.Tests/DataStructures/StackTests.cs
@@ -4,26 +4,29 @@ using DataStructuresAlgorithms.AbstractDataTypes;
 
 namespace DataStructuresAlgorithms.Tests.DataStructures
 {
-    public class StackTests
+    public abstract class StackTests
     {
+        protected abstract Stack<T> GetStackImplementation<T>();
+
         [Fact]
-        public void StackSinglyLinkedListTest0()
+        public void StackGeneralTest0()
         {
+            Stack<int> stack = GetStackImplementation<int>();
             var fixture = new { TestArray0 = new int[] { 3, 6, 9, 12 } };
-            Stack<int> stack = new StackSinglyLinkedList<int>();
             Assert.True(stack.IsEmpty);
             Assert.True(stack.Length == 0);
             Assert.Throws<InvalidOperationException>(() => stack.Pop());
             Assert.Throws<InvalidOperationException>(() => stack.Peek());
 
-            for(var i = 0; i < fixture.TestArray0.Length; i++)
+            for (var i = 0; i < fixture.TestArray0.Length; i++)
             {
                 Assert.True(stack.Length == i);
                 stack.Push(fixture.TestArray0[i]);
                 Assert.False(stack.IsEmpty);
+                Assert.True(stack.Peek() == fixture.TestArray0[i]);
             }
 
-            for(var i = fixture.TestArray0.Length - 1; i >= 0 ; i--)
+            for (var i = fixture.TestArray0.Length - 1; i >= 0; i--)
             {
                 Assert.False(stack.IsEmpty);
                 Assert.True(stack.Length == i + 1);
@@ -35,6 +38,14 @@ namespace DataStructuresAlgorithms.Tests.DataStructures
             Assert.True(stack.Length == 0);
             Assert.Throws<InvalidOperationException>(() => stack.Pop());
             Assert.Throws<InvalidOperationException>(() => stack.Peek());
+        }
+    }
+
+    public class StackSinglyLinkedListTests : StackTests
+    {
+        protected override Stack<T> GetStackImplementation<T>()
+        {
+            return new StackSinglyLinkedList<T>();
         }
     }
 }
