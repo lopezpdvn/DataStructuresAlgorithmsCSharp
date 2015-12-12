5607454bb91da869a6600869cd29773aa8fef367
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-12-11 23:45:28 -0600

    Work on SortedArrayInt

---
+++ src/DataStructuresAlgorithms/DataStructures/Array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace DataStructuresAlgorithms.DataStructures.Array
+{
+    public class SortedArrayInt : IEnumerable<int>
+    {
+        private int[] arr;
+        public int Length { get; private set; }
+
+        public SortedArrayInt(int length)
+        {
+            Length = length;
+            arr = new int[Length];
+        }
+
+        public int this[int index]
+        {
+            get { return arr[index]; }
+        }
+
+        public void Delete(int v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ tests/DataStructuresAlgorithms.Tests/DataStructures/ArrayTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+using DataStructuresAlgorithms.DataStructures.Array;
+
+namespace DataStructuresAlgorithms.Tests.DataStructures
+{
+    public class SortedArrayTests
+    {
+        [Fact]
+        public void SortedArrayTest0()
+        {
+            var a0 = new int[] { 1, 2, 3 };
+            //Array.BinarySearch()
+            var arrLength = 5;
+            var arr = new SortedArrayInt(arrLength);
+            foreach(var item in arr)
+            {
+                Assert.True(item == default(int));
+            }
+            Assert.Throws<IndexOutOfRangeException>(() => arr[arrLength]);
+            // Indexer is read only
+            //arr[0] = default(int);
+
+            Assert.True(arr.Length == 0);
+            arr.Delete(5);
+            arr.Delete(0);
+            Assert.True(arr.Length == 0);
+            Assert.True(arr.Length == 0);
+        }
+    }
+}
