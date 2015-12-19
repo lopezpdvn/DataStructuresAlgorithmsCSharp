c59726771c30344c403b87ba728fbbd02302ae71
Pedro Ivan Lopez <m@pedroivanlopez.com>
2015-12-19 04:09:13 -0600

    Created interfaces LinkedList and Node

--- src/DataStructuresAlgorithms/DataStructures/LinkedList.cs
+++ src/DataStructuresAlgorithms/DataStructures/LinkedList.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace DataStructuresAlgorithms.DataStructures.LinkedList
 {
-    public interface LinkedList<T> { }
+    public interface LinkedList<T> : IEnumerable<Node<T>>
+    {
+        Node<T> LastNode { get; }
+        Node<T> FirstNode { get; }
+        void Add(T value);
+        bool IsEmpty { get; }
+        int Length { get; }
+        int Count { get; }
 
-    public interface Node<T> { }
+        Node<T> AddFirst(T value);
+        Node<T> AddLast(T value);
+        Node<T> AddBefore(Node<T> node, T value);
+        Node<T> AddAfter(Node<T> node, T value);
+        bool Contains(Node<T> node);
+        void RemoveFirst();
+        void Remove(Node<T> node);
+        Node<int> Remove(int key);
+        void RemoveBefore(Node<T> node);
+        void RemoveAfter(Node<T> node);
+        void RemoveLast();
+        void Clear();
+        Node<T> Find(Node<T> node);
+        int Find(int key);
+        Node<T> FindPrevious(Node<T> node);
+        Node<int> FindPrevious(int key);
+        Node<T> FindMToLast(int m);
+        bool IsCyclic();
+    }
+
+    public interface Node<T>
+    {
+        Node<T> Next { get; }
+        Node<T> Prev { get; }
+        T Value { get; }
+    }
 }
