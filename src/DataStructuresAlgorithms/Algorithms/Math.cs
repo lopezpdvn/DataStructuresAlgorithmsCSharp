d952602632172434b3d9348b97423c234b969c89
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-01-01 20:13:54 -0600

    Implemented/tested Triangular numbers iterative & recursive

---
+++ src/DataStructuresAlgorithms/Algorithms/Math.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DataStructuresAlgorithms.Algorithms
+{
+    public static class Math
+    {
+        public static int TriangularIterative(int n)
+        {
+            if(n < 0)
+            {
+                throw new InvalidOperationException(
+                    "Argument is not natural number");
+            }
+            int total = 0;
+            while(n >= 0)
+            {
+                total += n--;
+            }
+            return total;
+        }
+
+        public static int TriangularRecursive(int n)
+        {
+            if(n < 0)
+            {
+                throw new InvalidOperationException(
+                    "Argument is not natural number");
+            }
+            return _TriangularRecursive(n);
+        }
+
+        private static int _TriangularRecursive(int n)
+        {
+            if(n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return n + _TriangularRecursive(n - 1);
+            }
+        }
+    }
+}
---
+++ tests/DataStructuresAlgorithms.Tests/Algorithms/Math.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Xunit;
+using DataStructuresAlgorithms.DataStructures.Graph;
+using MathAlgs = DataStructuresAlgorithms.Algorithms;
+
+namespace DataStructuresAlgorithms.Tests.Algorithms.Math
+{
+    public class MathTestsFixture
+    {
+        public readonly int[] TriangularNumbers = { 0, 1, 3, 6, 10, 15, 21,
+            28, 36, 45, 55, 66, 78, 91, 105, 120, 136, 153, 171, 190, 210,
+            231, 253, 276, 300, 325, 351, 378, 406 };
+    }
+
+    public abstract class MathTests
+    {
+        protected MathTestsFixture fixture;
+        protected Func<int, int> IntFunc;
+        protected string traversalType;
+        protected DirectedGraphAdjacencyList<char> graph;
+
+        [Fact]
+        public void TriangleTest0()
+        {
+            var i = 0;
+            foreach(var triangularNumber in fixture.TriangularNumbers)
+            {
+                Assert.True(triangularNumber == IntFunc(i++));
+            }
+        }
+    }
+
+    [CollectionDefinition("Math Tests Collection")]
+    public class MathTestsCollection : ICollectionFixture<MathTestsFixture> { }
+
+    [Collection("Math Tests Collection")]
+    public class TriangularNumbersIterative : MathTests
+    {
+        public TriangularNumbersIterative(MathTestsFixture fixture)
+        {
+            this.fixture = fixture;
+            IntFunc = MathAlgs.Math.TriangularIterative;
+        }
+    }
+
+    [Collection("Math Tests Collection")]
+    public class TriangularNumbersRecursive : MathTests
+    {
+        public TriangularNumbersRecursive(MathTestsFixture fixture)
+        {
+            this.fixture = fixture;
+            IntFunc = MathAlgs.Math.TriangularRecursive;
+        }
+    }
+}
