cce6e46345b035a4ef7c713a0934ed5ea6d2b16e
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-01-25 15:58:40 -0600

    Created shared fixture for Binary Tree tests

--- tests/DataStructuresAlgorithms.Tests/DataStructures/TreeTests.cs
+++ tests/DataStructuresAlgorithms.Tests/DataStructures/TreeTests.cs
@@ -4,9 +4,81 @@ using Xunit;
 using DataStructuresAlgorithms.DataStructures.Tree;
 using DataStructuresAlgorithms.DataStructures.Tree.BinaryTree;
 using DataStructuresAlgorithms.AbstractDataTypes;
+using System.Collections.Generic;
 
 namespace DataStructuresAlgorithms.Tests.DataStructures.Tree.BinaryTree
 {
+    public class BinaryTreeTraversalFixture
+    {
+        public BinaryTree<int> travTree0;
+        public IDictionary charTree = new Hashtable();
+
+        public BinaryTreeTraversalFixture()
+        {
+            /*
+           100
+          /  \
+         /    \
+        /      \
+       50      150
+      / \      /  \
+     25  75   125  175
+              /
+            110
+            */
+            travTree0 = new BinaryTree<int>(new Node<int>(
+
+                new Node<int>(
+                    new Node<int>(25), new Node<int>(75), 50),
+
+                new Node<int>(
+                    new Node<int>(
+                        new Node<int>(110), null, 125),
+                    new Node<int>(null, null, 175)
+
+                 , 150)
+
+                 , 100));
+
+            var _J = new Node<char>('J');
+            var _I = new Node<char>(_J, null, 'I');
+            var _H = new Node<char>(null, _I, 'H');
+            var _D = new Node<char>(_H, null, 'D');
+            var _M = new Node<char>(null, null, 'M');
+            var _L = new Node<char>(null, _M, 'L');
+            var _K = new Node<char>(_L, null, 'K');
+            var _E = new Node<char>(_K, null, 'E');
+            var _B = new Node<char>(_D, _E, 'B');
+            var _F = new Node<char>(null, null, 'F');
+            var _S = new Node<char>(null, null, 'S');
+            var _R = new Node<char>(_S, null, 'R');
+            var _N = new Node<char>(_R, null, 'N');
+            var _Q = new Node<char>(null, null, 'Q');
+            var _P = new Node<char>(null, _Q, 'P');
+            var _O = new Node<char>(null, _P, 'O');
+            var _G = new Node<char>(_N, _O, 'G');
+            var _C = new Node<char>(_F, _G, 'C');
+            var _A = new Node<char>(_B, _C, 'A');
+            var _charTree = new BinaryTree<char>(_A);
+            charTree["tree"] = _charTree;
+            charTree["bf_traversal_char_array"] =
+                "ABCDEFGHKNOILRPJMSQ".ToCharArray();
+        }
+    }
+
+    public abstract class BinaryTreeTraversalTests
+    {
+        protected BinaryTreeTraversalFixture fixture;
+        protected Func<INode<char>, IEnumerable<INode<char>>>
+            TraversalAlgorithm;
+
+        [Fact]
+        public void TraversalTest()
+        {
+
+        }
+    }
+
     public class BinaryTreeTests
     {
         BinaryTree<int> travTree0;
