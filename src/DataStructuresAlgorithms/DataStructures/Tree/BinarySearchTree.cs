1e6a1fd574bcc7da9a12649cc36f30242ade3b52
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-09-16 17:13:55 -0500

    Implemented `BinarySearchTreeInt.Insert`

--- src/DataStructuresAlgorithms/DataStructures/Tree/BinarySearchTree.cs
+++ src/DataStructuresAlgorithms/DataStructures/Tree/BinarySearchTree.cs
@@ -1,12 +1,52 @@
-﻿namespace DataStructuresAlgorithms.DataStructures.Tree.BinaryTree.BinarySearchTree
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStructuresAlgorithms.DataStructures.Tree.BinaryTree.BinarySearchTree
 {
     public class BinarySearchTreeInt
     {
+        public NodeInt Root { get; private set; }
 
+        public void Insert(NodeInt newNode)
+        {
+            if(Root == null)
+            {
+                Root = newNode;
+                return;
+            }
+            NodeInt parent = null, node = Root;
+            while(true)
+            {
+                parent  = node;
+                if(newNode < node)
+                {
+                    node = (NodeInt)node.Left;
+                    if(node == null)
+                    {
+                        parent.Left = newNode;
+                        return;
+                    }
+                }
+                else
+                {
+                    node = (NodeInt)node.Right;
+                    if(node == null)
+                    {
+                        parent.Right = newNode;
+                        return;
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<NodeInt> IODFTraversal()
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class NodeInt : Node<int>
-    {
+    {   
         public NodeInt(int value = 0) : base(null, null, value) {}
 
         public static bool operator < (NodeInt a, NodeInt b)
@@ -21,10 +61,10 @@
         public static bool operator > (NodeInt a, NodeInt b)
             => a.Value > b.Value;
 
-        public static bool operator == (NodeInt a, NodeInt b)
-            => a.Value == b.Value;
+        //public static bool operator == (NodeInt a, NodeInt b)
+          //  => a.Value == b.Value;
 
-        public static bool operator != (NodeInt a, NodeInt b)
-            => a.Value != b.Value;
+        //public static bool operator != (NodeInt a, NodeInt b)
+          //  => a.Value != b.Value;
     }
 }
--- tests/DataStructuresAlgorithms.Tests/DataStructures/BinarySearchTreeTests.cs
+++ tests/DataStructuresAlgorithms.Tests/DataStructures/BinarySearchTreeTests.cs
@@ -6,6 +6,23 @@ namespace DataStructuresAlgorithms.Tests.DataStructures.Tree.BinarySearchTree
     public class BinarySearchTreeIntTests
     {
         [Fact]
+        public void MiscTest0()
+        {
+            int[] keys = { 99, -9, 0, -99, -999, 999, 9 };
+            var bstInt = new BinarySearchTreeInt();
+            foreach(var i in keys)
+            {
+                bstInt.Insert(new NodeInt(i));
+            }
+            System.Array.Sort(keys);
+            System.Console.WriteLine();
+            /*foreach(var i in bstInt.IODFTraversal())
+            {
+                System.Console.Write(i + " ");
+            }*/
+        }
+
+        [Fact]
         public void NodeIntTests0()
         {
             var smallest = new NodeInt(-99);
